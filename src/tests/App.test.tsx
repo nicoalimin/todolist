e97fb6091a219c5085d2b1b454078cd9a81f5e42
nicoalimin <nicoalimin@yahoo.com>
2020-04-02 10:54:55 -0700

    Added unit tests to App

---
+++ src/tests/App.test.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "../App";
+
+// Checks whether the app renders properly
+test("renders learn react link", () => {
+  render(<App />);
+});
